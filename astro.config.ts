f82353db1d750c46c702693441078a3d70779bcd
lbahamonde <luis.bahamonde@hey.com>
2025-05-05 19:58:10 -0400

    ADD / as base

--- astro.config.ts
+++ astro.config.ts
@@ -11,6 +11,7 @@ import { spectreDark } from './src/ec-theme';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lbahamonde.github.io',
+  base: '/',
   output: 'static',
   outDir: './docs',
   integrations: [
